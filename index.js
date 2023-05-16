10cad91b45dca8b9d5593a428e78a6459a80a771
Taehyuk <7374179@gmail.com>
2023-05-15 18:09:48 -0700

    try to modify

--- index.js
+++ index.js
@@ -45,139 +45,6 @@ app.get('/', (req, res) => {
     res.render("main");
 });
 
-app.get('/recipe/:name', async (req, res) => {
-    const recipeName = req.params.name;
-    const userIngredients = req.query.userIngredients || '';
-    const messages = [
-      { role: 'system', content: 'You are a helpful assistant that provides detailed instructions for a given recipe.' },
-      { role: 'user', content: `Provide step-by-step instructions on how to make ${recipeName}. Do not give the step number.` },
-    ];
-  
-    try {
-      const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-      });
-  
-      const completionText = completion.data.choices[0].message.content;
-      const instructionsRegex = /Instructions:(.*)/s;
-  
-      const instructionsMatch = completionText.match(instructionsRegex);
-  
-      if (!instructionsMatch) {
-        // Handle the case where the regex didn't match any text
-        res.status(500).send('Error retrieving instructions from the completion text.');
-        return;
-      }
-  
-      const instructionsText = instructionsMatch[1].trim().split('\n').filter(line => line.trim() !== '');
-
-      const instructions = [];
-      let currentStep = 1;
-  
-      instructionsText.forEach(line => {
-        const stepMatch = line.match(/^\d+/);
-        if (stepMatch) {
-          // Line starts with a step number, update the current step
-          currentStep = parseInt(stepMatch[0]);
-          const instruction = line.replace(/^\d+\.\s/, '').trim();
-          instructions.push({ step: currentStep, instruction });
-        } else if (instructions.length > 0) {
-          // Line doesn't start with a step number, append it to the previous instruction
-          instructions[instructions.length - 1].instruction += ' ' + line.trim();
-        }
-      });
-  
-  
-      res.render('recipe', {
-        name: recipeName,
-        instructions: instructionsText,
-        userIngredients: req.query.userIngredients || '',
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Error retrieving recipe instructions.');
-    }
-  });
-  
-
-
-
-  app.get('/shopping-list/:name', async (req, res) => {
-    const recipeName = req.params.name;
-  
-    // Get the userIngredients from the query string
-    const userIngredients = req.query.userIngredients
-      ? req.query.userIngredients.split(',')
-      : [];
-  
-    const messages = [
-      {
-        role: 'system',
-        content:
-          'You are a helpful assistant that provides detailed instructions for a given recipe.',
-      },
-      {
-        role: 'user',
-        content: `Provide a list of ingredients need to buy for ${recipeName}.`,
-      },
-    ];
-  
-    try {
-      const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-      });
-  
-      const completionText = completion.data.choices[0].message.content;
-      console.log(completionText); // Add this line to print the completion text
-  
-      const ingredientsRegex = /(-.*\n)+/g;
-
-      
-  
-      const ingredientsMatch = completionText.match(ingredientsRegex);
-  
-      if (!ingredientsMatch) {
-        // Handle the case where the regex didn't match any text
-        res.status(500).send('Error retrieving ingredients from the completion text.');
-        return;
-      }
-  
-      const ingredientsText = ingredientsMatch[1].trim().split('\n');
-  
-      // Filter the ingredients to generate the shopping list
-      const shoppingList = ingredientsText.filter(
-        (ingredient) => !userIngredients.includes(ingredient)
-      );
-  
-      res.render('shopping-list', {
-        name: recipeName,
-        shoppingList: shoppingList,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Error retrieving recipe instructions.');
-    }
-  });
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
@@ -360,19 +227,26 @@ app.post('/submitUser', async (req, res) => {
 
     var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await userCollection.insertOne({
-        email: email,
-        username: username,
-        password: hashedPassword,
-        user_type: "user",
-        status_user: status_user
-    });
-    console.log("Inserted user");
+    try {
+        await userCollection.insertOne({
+            email: email,
+            username: username,
+            password: hashedPassword,
+            user_type: "user",
+            status_user: status_user,
+            bookmarks:[]
+        });
+        console.log("Inserted user");
 
-    req.session.authenticated = true;
-    req.session.email = email;
-    req.session.cookie.maxAge = expireTime;
-    res.redirect('/members');
+        req.session.authenticated = true;
+        req.session.email = email;
+        req.session.cookie.maxAge = expireTime;
+        res.redirect('/members');
+    } catch (error) {
+        console.error("Failed to insert user: ", error);
+        // Handle the error here. You may want to redirect to an error page or show a message to the user.
+        res.status(500).send("Error creating user.");
+    }
 });
 
 app.post('/loggingin', async (req, res) => {
@@ -662,31 +536,147 @@ app.post('/saveProfile', async (req, res) => {
     }
 });
 
+app.get('/recipe/:name', async (req, res) => {
+    const recipeName = req.params.name;
+    const userIngredients = req.query.userIngredients || '';
+    const messages = [
+      { role: 'system', content: 'You are a helpful assistant that provides detailed instructions for a given recipe.' },
+      { role: 'user', content: `Provide step-by-step instructions on how to make ${recipeName}. Do not give the step number.` },
+    ];
+  
+    try {
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+      });
+  
+      const completionText = completion.data.choices[0].message.content;
+      const instructionsRegex = /Instructions:(.*)/s;
+  
+      const instructionsMatch = completionText.match(instructionsRegex);
+  
+      if (!instructionsMatch) {
+        // Handle the case where the regex didn't match any text
+        res.status(500).send('Error retrieving instructions from the completion text.');
+        return;
+      }
+  
+      const instructionsText = instructionsMatch[1].trim().split('\n').filter(line => line.trim() !== '');
 
-app.post('/bookmarks/add', sessionValidation, async (req, res) => {
-    if (req.session.authenticated) {
-        try {
-            const {title, url} = req.body;   // ???
-            const userId = req.session.userId;
-
-            await database.db(mongodb_database).collection('bookmarks').insertOne({
-                    username: username,
-                    title: title,
-                    url: url
-                }
-            );
-            console.log("Inserted user");
-
-
-            res.status(200).send('Bookmark added successfully');
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('Internal server error');
+      const instructions = [];
+      let currentStep = 1;
+  
+      instructionsText.forEach(line => {
+        const stepMatch = line.match(/^\d+/);
+        if (stepMatch) {
+          // Line starts with a step number, update the current step
+          currentStep = parseInt(stepMatch[0]);
+          const instruction = line.replace(/^\d+\.\s/, '').trim();
+          instructions.push({ step: currentStep, instruction });
+        } else if (instructions.length > 0) {
+          // Line doesn't start with a step number, append it to the previous instruction
+          instructions[instructions.length - 1].instruction += ' ' + line.trim();
         }
-    } else {
-        res.status(401).send('Unauthorized');
+      });
+  
+  
+      res.render('recipe', {
+        name: recipeName,
+        instructions: instructionsText,
+        userIngredients: req.query.userIngredients || '',
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Error retrieving recipe instructions.');
     }
-})
+  });
+  
+  app.get('/shopping-list/:name', async (req, res) => {
+    const recipeName = req.params.name;
+  
+    // Get the userIngredients from the query string
+    const userIngredients = req.query.userIngredients
+      ? req.query.userIngredients.split(',')
+      : [];
+  
+    const messages = [
+      {
+        role: 'system',
+        content:
+          'You are a helpful assistant that provides detailed instructions for a given recipe.',
+      },
+      {
+        role: 'user',
+        content: `Provide a list of ingredients need to buy for ${recipeName}.`,
+      },
+    ];
+  
+    try {
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+      });
+  
+      const completionText = completion.data.choices[0].message.content;
+      console.log(completionText); // Add this line to print the completion text
+  
+      const ingredientsRegex = /(-.*\n)+/g;
+
+      
+  
+      const ingredientsMatch = completionText.match(ingredientsRegex);
+  
+      if (!ingredientsMatch) {
+        // Handle the case where the regex didn't match any text
+        res.status(500).send('Error retrieving ingredients from the completion text.');
+        return;
+      }
+  
+      const ingredientsText = ingredientsMatch[1].trim().split('\n');
+  
+      // Filter the ingredients to generate the shopping list
+      const shoppingList = ingredientsText.filter(
+        (ingredient) => !userIngredients.includes(ingredient)
+      );
+  
+      res.render('shopping-list', {
+        name: recipeName,
+        shoppingList: shoppingList,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Error retrieving recipe instructions.');
+    }
+  });
+
+
+  app.post('/bookmarks/add', sessionValidation, async (req, res) => {
+    if (req.session.authenticated) {
+      try {
+        const { title, instructions, url, isBookmarked } = req.body;        
+        const userId = req.session.userId;
+        const userEmail = req.session.email;
+
+
+        // Update the user's bookmarks array
+    const result = await userCollection.updateOne(
+        { email: userEmail },
+        // { $push: { bookmarks: bookmark } }
+        { $push: { bookmarks: { title, instructions, url, isBookmarked } } }
+
+      );
+      console.log(result);
+
+        console.log("Inserted bookmark");
+        res.status(200).send('Bookmark added successfully');
+      } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal server error');
+      }
+    } else {
+      res.status(401).send('Unauthorized');
+    }
+  });
 
 app.get('/change_password', (req, res) => {
     if(!req.session.forgotPassword){
@@ -713,23 +703,68 @@ app.post('/change_password', async (req, res) => {
         res.render('change_password', { error });
     }
 });
+
+
+// app.post('/bookmarks', sessionValidation, async (req, res) => {
+//     if (!req.session.authenticated) {
+//         return res.status(403).send('Not authenticated');
+//     }
+
+//     try {
+//         const {title} = req.body;
+        
+//         await database.db(mongodb_database).collection('bookmarks').insertOne({
+//             userId: req.session.userId,
+//             title: title
+//         });
+
+//         res.status(200).send('Bookmarked');
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send('Error bookmarking recipe');
+//     }
+// });
+
+// app.delete('/bookmarks', sessionValidation, async (req, res) => {
+//     if (!req.session.authenticated) {
+//       return res.status(403).send('Not authenticated');
+//     }
+  
+//     try {
+//       const { title } = req.body;
+  
+//       await database.db(mongodb_database).collection('bookmarks').deleteOne({
+//         userId: req.session.userId,
+//         title: title
+//       });
+  
+//       res.status(200).send('Unbookmarked');
+//     } catch (error) {
+//       console.log(error);
+//       res.status(500).send('Error unbookmarking recipe');
+//     }
+//   });
+
+
 app.get('/bookmarks', sessionValidation, async (req, res) => {
     if (req.session.authenticated) {
-        //   try {
-        //     const userId = req.session.userId;
-
-        //     const bookmarks = await database.db(mongodb_database).collection('bookmarks').find({ userId }).toArray();
-
-        //     res.status(200).json(bookmarks);
-        //   } catch (error) {
-        //     console.log(error);
-        //     res.status(500).send('Internal server error');
-        //   }
-        res.render("bookmarks")
+      try {
+        const userEmail = req.session.email;
+        const user = await userCollection.findOne({ email: userEmail });
+  
+        if (user) {
+          res.render('bookmarks', { bookmarks: user.bookmarks });
+        } else {
+          res.status(404).send('User not found');
+        }
+      } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal server error');
+      }
     } else {
-        res.status(401).send('Unauthorized');
+      res.status(401).send('Unauthorized');
     }
-});
+  });
 
 app.get('/ingredientsList', sessionValidation, async (req, res) => {
     if (req.session.authenticated) {
